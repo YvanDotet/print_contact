3f23ffdb1c54ba39c24862a30c63f5fd24f085fe
YvanDotet <40297112+YvanDotet@users.noreply.github.com>
2020-05-01 14:31:44 +0200

    Update print_call.py

--- print_contact/models/print_call.py
+++ print_contact/models/print_call.py
@@ -5,15 +5,12 @@ from odoo.exceptions import Warning,ValidationError,UserError
 class extend_contact(models.Model):
     _inherit='res.partner'
     
-    @api.multi
     def print_page(self):
         return self.env.ref('print_contact.action_report_page').report_action(self)
     
-    @api.multi
     def print_listing(self):
         return self.env.ref('print_contact.action_report_listing').report_action(self)
     
-    @api.multi
     def print_checkin(self):
         return self.env.ref('print_contact.action_report_checkin').report_action(self)
     
