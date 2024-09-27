b5d9b57fd890eda8536025004afc5cc33125561d
YvanDotet <40297112+YvanDotet@users.noreply.github.com>
2024-09-28 00:06:36 +0200

    Add files via upload

---
+++ print_contact/models/print_call.py
@@ -0,0 +1,15 @@
+from odoo import fields,models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    def print_page(self):
+        return self.env.ref('print_contact.action_report_page').report_action(self)
+    
+    def print_listing(self):
+        return self.env.ref('print_contact.action_report_listing').report_action(self)
+    
+    def print_checkin(self):
+        return self.env.ref('print_contact.action_report_checkin').report_action(self)
+    
