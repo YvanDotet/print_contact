3845ebd81f2a2f7bebbe40e37e79d3b25e4c38ce
YvanDotet <40297112+YvanDotet@users.noreply.github.com>
2025-10-08 00:18:28 +0200

    Add files via upload

---
+++ print_contact/__manifest__.py
@@ -0,0 +1,21 @@
+{
+        'name':'Printing of contacts',
+        'description':'Provide contacts pdfs reports',
+        'summary':'Many PDFs available for provide contacts reports.',
+        'author':'Yvan Dotet',
+        'depends':['base','contacts'],
+        'application':False,        
+        'version':'19.0.0.1',
+        'license':'AGPL-3',
+        'support': 'yvandotet@yahoo.fr',
+        'website':'https://github.com/YvanDotet/print_contact',
+        
+        'data':[
+            'report/print_page.xml',
+            'report/print_listing.xml',
+            'report/print_checkin.xml',
+            ],
+         
+        'images': ['images/thumbnail.png'],
+}
+
